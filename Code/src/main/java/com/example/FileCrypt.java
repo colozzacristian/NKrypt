44b8712e889d3200a1f96fcd40a8da00ad5d21d6
navadenis <154016218+navadenis@users.noreply.github.com>
2024-04-25 20:43:31 +0200

    Updated FileCrypt.java
    New verify for a right decryption (after copying the file to ensure we don't lose it)

--- Code/src/main/java/com/example/FileCrypt.java
+++ Code/src/main/java/com/example/FileCrypt.java
@@ -5,6 +5,7 @@
 package com.example;
 
 import java.io.File;
+import java.util.Scanner;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -32,9 +33,11 @@ public class FileCrypt {
     private String filepath;
     private File file;
     private byte [] byte_file;
+    private File copy_file;
+    private String verifica = "Encryption";
 
-    public FileCrypt(String chiave, String nome_file) {
-        filepath="cartella/nome_file";
+    public FileCrypt(String chiave, String path_file) {
+        this.filepath=path_file;
         this.file = new File (filepath);
         key = new SecretKeySpec(chiave.getBytes(), SYM_ALGORITHM);
     }
@@ -46,35 +49,51 @@ public class FileCrypt {
         byte_file=Files.readAllBytes(Paths.get(filepath));
     }
     
-    public void encryption() throws
-        NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, 
-        IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException {
-        byte [] iv = inizializza_byte();
+    public void encryption() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException {
+        byte [] iv = inizializza_byte(iv);
         Cipher cipher = Cipher.getInstance( "AES/CBC/PKCS5Padding" );
         cipher.init( Cipher.ENCRYPT_MODE, key, new IvParameterSpec( iv ) );
         this.key=new SecretKeySpec("0000000".getBytes(), SYM_ALGORITHM); //cambio la chiave per non rendere più disponibile la vecchia chiave (volendo)
         byte [] new_byte = cipher.doFinal( byte_file );
-        Path path = Paths.get("cartella/"+filepath);
+        Path path = Paths.get(filepath);
         Files.write(path, new_byte);
+        
+        FileWriter fileWriter = new FileWriter(filePath, true); // true per appendere al file
+        BufferedWriter writer = new BufferedWriter(fileWriter);
+        writer.write(this.verifica);
+        writer.newLine();
+        writer.close();
     }
     
-    public static byte [] inizializza_byte() {
-    SecureRandom random = new SecureRandom();
-    byte [] iv = new byte [ SYM_KEY_SIZE / 8 ];
-    random.nextBytes( iv );
-    return iv;
-}
+    public byte [] inizializza_byte(byte [] iv) {
+        SecureRandom random = new SecureRandom();
+        iv = new byte [ SYM_KEY_SIZE / 8 ];
+        random.nextBytes( iv );
+        return iv;
+    }
     
     
-    public void decryption(String chiave) throws
-    NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, 
-        IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException {
+    public boolean decryption(String chiave) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException {
+        this.copy_file = new File ("copy_"+filepath);
+        this.copy_file = this.file; //copia nel caso la decriptazione avvenga con la chiave errata
+        Scanner reader;
         byte [] iv = inizializza_byte();
         Cipher cipher = Cipher.getInstance( "AES/CBC/PKCS5Padding" );
         key = new SecretKeySpec(chiave.getBytes(), SYM_ALGORITHM);
         cipher.init( Cipher.ENCRYPT_MODE, key, new IvParameterSpec( iv ) );
         byte [] new_byte = cipher.doFinal( byte_file );
-        Path path = Paths.get("cartella/"+filepath);
+        Path path = Paths.get(filepath);
         Files.write(path, new_byte);
+        
+        reader = new Scanner(this.file);
+        String str_verifica = "";
+        while (scanner.hasNextLine()) {
+            str_verifica = scanner.nextLine();
+        }
+        scanner.close();
+        if (this.verifica.equals(str_verifica)) return true; //ritorno true se la chiave fornita è corretta e dunque è stata eseguita una decriptazione corretta, in caso contrario ritorno false e si ripeterà il login
+
+        this.File = this.copy_file;
+        return false;
     }
 }
