ee75f384b5c7a825dcb6136f9b123907be13be5a
navadenis <nvadns06e24d416i@iisbadoni.edu.it>
2024-05-01 13:16:07 +0200

    Updating MainUiController

--- Code/src/main/java/com/example/Crypto.java
+++ Code/src/main/java/com/example/Crypto.java
@@ -1,37 +1,58 @@
 package com.example;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Crypto {
-    private String name;
+
     private double price;
     private double quantity;
+    private StringProperty nameProperty;
+    private StringProperty priceProperty;
+    private StringProperty quantityProperty;
 
+    public Crypto(String name, double price, double quantity) {
+        this.price = price;
+        this.quantity = quantity;
+        this.nameProperty = new SimpleStringProperty(name);
+        this.priceProperty = new SimpleStringProperty(price+"");
+        this.quantityProperty = new SimpleStringProperty(quantity+"");
+    }
+
+    public StringProperty getNameProperty() {
+        return nameProperty;
+    }
 
     public String getName() {
-        return name;
+        return nameProperty.get();
     }
-    public void setName(String name) {
-        this.name = name;
+
+    public StringProperty getPriceProperty() {
+        return nameProperty;
     }
+
+    public StringProperty getQuantityProperty() {
+        return nameProperty;
+    }
+
     public double getPrice() {
         return price;
     }
+
     public void setPrice(double price) {
         this.price = price;
     }
+
     public double getQuantity() {
         return quantity;
     }
+
     public void setQuantity(double quantity) {
         this.quantity = quantity;
     }
 
-    public double getAssetValue(){
-        return price*quantity;
-    }
-    public Crypto(String name, double price, double quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
+    public StringProperty getAssetValue(){
+        return new SimpleStringProperty(price*quantity+"");
     }
     
 }
