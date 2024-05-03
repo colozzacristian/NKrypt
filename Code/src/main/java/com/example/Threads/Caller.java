b954e546be27da4c6660a88591a6a31b1e61f887
Colozza Cristian <133865338+colozzacristian@users.noreply.github.com>
2024-05-03 23:36:15 +0200

    update

--- Code/src/main/java/com/example/Threads/Caller.java
+++ Code/src/main/java/com/example/Threads/Caller.java
@@ -7,7 +7,7 @@ import com.example.CryptoList;
 
 public class Caller extends Thread{
     String baseUrl="https://min-api.cryptocompare.com/data/pricemulti?fsyms=";
-    String append2="&tsyms=EUR&api_key=8834b8ab09b3a21eac9fb72abab5a866ec3facab20e17a826a1c8d004cc9338d";
+    String append2="&tsyms=EUR&api_key=8834b8ab09b3a21eac9fb72abab5b20e17a826a1c8d004cc";
     CryptoList cryptoList;
     boolean isAlive=true;
     //API KEY in URL - just append ? or &api_key={your_api_key} the the end of your request url
